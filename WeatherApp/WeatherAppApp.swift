cd047b7b4928193b4629bedb9c8c9e0690e2ef5e
Bastien <contact@bastienguillet.fr>
2021-03-13 15:34:16 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Naomi Duclaux on 13/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
